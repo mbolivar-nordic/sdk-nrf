b77386089f22e0aed3f6071e9024b7036351c406
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-08-03 19:20:43 -0700

    treewide: remove unused pm_device_state_set callback args
    Zephyr commit 26ad8376bd5a9dca7b225a7b7baa9c332e876fb8
    ("pm: remove callback from control function") removed the callback and
    its argument from the pm_device_state_set() API. Keep up.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/shell.c
+++ samples/nrf9160/modem_shell/src/shell.c
@@ -215,12 +215,12 @@ static void disable_uarts(void)
 
 	uart_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart0)));
 	if (uart_dev) {
-		pm_device_state_set(uart_dev, PM_DEVICE_STATE_LOW_POWER, NULL, NULL);
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_LOW_POWER);
 	}
 
 	uart_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart1)));
 	if (uart_dev) {
-		pm_device_state_set(uart_dev, PM_DEVICE_STATE_LOW_POWER, NULL, NULL);
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_LOW_POWER);
 	}
 }
 
@@ -230,12 +230,12 @@ static void enable_uarts(void)
 
 	uart_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart0)));
 	if (uart_dev) {
-		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE);
 	}
 
 	uart_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart1)));
 	if (uart_dev) {
-		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE);
 	}
 }
 
